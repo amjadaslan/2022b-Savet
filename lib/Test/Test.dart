23c99286b145296a1e04a70d3279073f5e43395e
mzareeb99 <mzareeb99@hotmail.com>
2022-06-12 11:28:27 +0300

    #37 working on reminder

--- lib/Test/Test.dart
+++ lib/Test/Test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:localpushnotifications/widget/local_notification_widget.dart';
 
 class test extends StatefulWidget {
   const test({Key? key}) : super(key: key);
@@ -8,8 +9,32 @@ class test extends StatefulWidget {
 }
 
 class _testState extends State<test> {
+  final String appTitle = 'Local Notifications';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: SingleChildScrollView(
+            child: Center(
+                child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: Column(
+                      children: <Widget>[
+                        const SizedBox(height: 20),
+                        const Text(''),
+                        const SizedBox(height: 20),
+                        Padding(
+                            padding: const EdgeInsets.only(),
+                            child: TextButton(
+                                child: const Text(
+                                  "local notification",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                onPressed: () {})),
+                      ],
+                    )))));
   }
 }
