1ad61cc8f6113cf58fd3336d28563cc7ce9bf818
Amjad <amjad_aslan@Hotmail.com>
2022-06-24 22:43:20 +0300

    fixed explore page number of likes & moved report to 3 dots menu & added back like and comment buttons

--- lib/Explore/explore_card.dart
+++ lib/Explore/explore_card.dart
@@ -2,6 +2,7 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:savet/Posts/Post/postPage.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 import '../Posts/Post/post_comment_section.dart';
@@ -9,8 +10,10 @@ import '../Posts/similar_content_card.dart';
 import '../Services/user_db.dart';
 
 class explore_card extends StatefulWidget {
-  explore_card({Key? key, required this.post}) : super(key: key);
+  explore_card({Key? key, required this.post, required this.user})
+      : super(key: key);
   Map post;
+  Map user;
   @override
   _explore_cardState createState() => _explore_cardState();
 }
@@ -36,11 +39,126 @@ class _explore_cardState extends State<explore_card> {
                   borderRadius: BorderRadius.circular(20),
                   child: Column(
                     children: [
-                      Container(
-                        color: Colors.white,
-                        child: FadeInImage.assetNetwork(
-                            placeholder: 'assets/preloader.gif',
-                            image: (this.widget.post['image'])),
+                      Stack(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => postPage(
+                                          cat_id: widget.post['cat_id'],
+                                          post_id: widget.post['id'],
+                                          user: widget.user)));
+                            },
+                            child: Container(
+                              color: Colors.white,
+                              child: FadeInImage.assetNetwork(
+                                  placeholder: 'assets/preloader.gif',
+                                  image: (this.widget.post['image'])),
+                            ),
+                          ),
+                          Positioned(
+                            top: -5.0,
+                            right: -10.0,
+                            child: PopupMenuButton(
+                                icon: Icon(Icons.more_vert, color: Colors.grey),
+                                itemBuilder: (context) => [
+                                      PopupMenuItem(
+                                        child: TextButton(
+                                          onPressed: () {
+                                            showDialog(
+                                                context: context,
+                                                barrierDismissible:
+                                                    false, // user must tap button!
+                                                builder:
+                                                    (BuildContext context) {
+                                                  return AlertDialog(
+                                                    title: Text('Report post'),
+                                                    content:
+                                                        SingleChildScrollView(
+                                                      child: Column(
+                                                        children: const <
+                                                            Widget>[
+                                                          Text(
+                                                              'Are you sure you want to report this post?')
+                                                        ],
+                                                      ),
+                                                    ),
+                                                    actions: [
+                                                      TextButton(
+                                                          child: Text('Report'),
+                                                          style: ButtonStyle(
+                                                              foregroundColor:
+                                                                  MaterialStateProperty
+                                                                      .all(Colors
+                                                                          .white),
+                                                              backgroundColor:
+                                                                  MaterialStateProperty
+                                                                      .all(Colors
+                                                                          .deepOrange)),
+                                                          onPressed: () {
+                                                            widget.post[
+                                                                    'image'] =
+                                                                'https://firebasestorage.googleapis.com/v0/b/savet-b9216.appspot.com/o/report.png?alt=media&token=ab9ee150-0bd4-4697-aee9-96b10d7b4959';
+                                                            Provider.of<UserDB>(
+                                                                    context,
+                                                                    listen:
+                                                                        false)
+                                                                .addToReported(
+                                                                    widget.post[
+                                                                        'id']);
+                                                            setState(() {});
+                                                            Navigator.of(
+                                                                    context)
+                                                                .pop(true);
+                                                          }),
+                                                      TextButton(
+                                                        child: Text('Cancel'),
+                                                        style: ButtonStyle(
+                                                            foregroundColor:
+                                                                MaterialStateProperty
+                                                                    .all(Colors
+                                                                        .white),
+                                                            backgroundColor:
+                                                                MaterialStateProperty
+                                                                    .all(Colors
+                                                                        .deepOrange)),
+                                                        onPressed: () =>
+                                                            Navigator.of(
+                                                                    context)
+                                                                .pop(false),
+                                                      ),
+                                                    ],
+                                                  );
+                                                });
+                                          },
+                                          child: Row(
+                                            children: const [
+                                              Icon(Icons.report),
+                                              Text("Report")
+                                            ],
+                                          ),
+                                          //prefixIcon: Icon(Icons.add_alert),
+                                          // child: Container(
+                                          //   decoration: BoxDecoration(),
+                                          //   child: Center(
+                                          //     child: Text(
+                                          //       'Edit',
+                                          //       style: TextStyle(
+                                          //         fontFamily: 'Arial',
+                                          //         fontSize: 18,
+                                          //         color: Colors.deepOrange,
+                                          //       ),
+                                          //       textAlign: TextAlign.center,
+                                          //     ),
+                                          //   ),
+                                          // ),
+                                        ),
+                                      ),
+                                    ]),
+                          )
+                        ],
                       ),
                       Container(
                         height: 40,
@@ -51,6 +169,7 @@ class _explore_cardState extends State<explore_card> {
                             Column(
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
+                                SizedBox(width: 5),
                                 Text(
                                   "  ${widget.post['username']}",
                                   style: TextStyle(
@@ -61,12 +180,12 @@ class _explore_cardState extends State<explore_card> {
                                 ),
                                 Row(
                                   children: [
-                                    SizedBox(width: 10),
+                                    SizedBox(width: 5),
                                     Icon(Icons.favorite,
-                                        color: Colors.red, size: 10),
+                                        color: Colors.red, size: 13),
                                     SizedBox(width: 2),
                                     Text(
-                                      "1,828 Likes",
+                                      "${widget.post['likes'] + widget.post['loves']} Likes",
                                       style: TextStyle(
                                           fontFamily: 'arial',
                                           decoration: TextDecoration.none,
@@ -83,92 +202,31 @@ class _explore_cardState extends State<explore_card> {
                                   IconButton(
                                       iconSize: 15,
                                       onPressed: () {
-                                        showDialog(
-                                          context: context,
-                                          barrierDismissible:
-                                              false, // user must tap button!
-                                          builder: (BuildContext context) {
-                                            return AlertDialog(
-                                              title: Text('Report post'),
-                                              content: SingleChildScrollView(
-                                                child: Column(
-                                                  children: const <Widget>[
-                                                    Text(
-                                                        'Are you sure you want to report this post?')
-                                                  ],
-                                                ),
-                                              ),
-                                              actions: [
-                                                TextButton(
-                                                    child: Text('Report'),
-                                                    style: ButtonStyle(
-                                                        foregroundColor:
-                                                            MaterialStateProperty
-                                                                .all(Colors
-                                                                    .white),
-                                                        backgroundColor:
-                                                            MaterialStateProperty
-                                                                .all(Colors
-                                                                    .deepOrange)),
-                                                    onPressed: () {
-                                                      widget.post['image'] =
-                                                          'https://firebasestorage.googleapis.com/v0/b/savet-b9216.appspot.com/o/report.png?alt=media&token=ab9ee150-0bd4-4697-aee9-96b10d7b4959';
-                                                      Provider.of<UserDB>(
-                                                              context,
-                                                              listen: false)
-                                                          .addToReported(widget
-                                                              .post['id']);
-                                                      setState(() {});
-                                                      Navigator.of(context)
-                                                          .pop(true);
-                                                    }),
-                                                TextButton(
-                                                  child: Text('Cancel'),
-                                                  style: ButtonStyle(
-                                                      foregroundColor:
-                                                          MaterialStateProperty
-                                                              .all(
-                                                                  Colors.white),
-                                                      backgroundColor:
-                                                          MaterialStateProperty
-                                                              .all(Colors
-                                                                  .deepOrange)),
-                                                  onPressed: () =>
-                                                      Navigator.of(context)
-                                                          .pop(false),
-                                                ),
-                                              ],
-                                            );
-                                          },
-                                        );
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    post_comment_section(
+                                                        post_id:
+                                                            widget.post['id'],
+                                                        cat_id: widget
+                                                            .post['cat_id'],
+                                                        user: widget.user)));
                                       },
-                                      icon: Icon(Icons.report,
+                                      icon: Icon(Icons.mode_comment_outlined,
                                           color: Colors.grey[400])),
-                                  // IconButton(
-                                  //     iconSize: 15,
-                                  //     onPressed: () {
-                                  //       // Navigator.push(
-                                  //       //     context,
-                                  //       //     MaterialPageRoute(
-                                  //       //         builder: (context) =>
-                                  //       //             public_post_comments(
-                                  //       //                 post_id: 0,
-                                  //       //                 cat_id: 0)));
-                                  //     },
-                                  //     icon: Icon(Icons.mode_comment_outlined,
-                                  //         color: Colors.grey[400])),
-                                  // IconButton(
-                                  //     iconSize: 18,
-                                  //     onPressed: () {
-                                  //       setState(() {
-                                  //         isPressed = !isPressed;
-                                  //       });
-                                  //     },
-                                  //     icon: (!isPressed)
-                                  //         ? Icon(Icons.favorite_border,
-                                  //             color: Colors.grey[400])
-                                  //         : Icon(Icons.favorite,
-                                  //             color: Colors.red))
+                                  IconButton(
+                                      iconSize: 18,
+                                      onPressed: () {
+                                        setState(() {
+                                          isPressed = !isPressed;
+                                        });
+                                      },
+                                      icon: (!isPressed)
+                                          ? Icon(Icons.favorite_border,
+                                              color: Colors.grey[400])
+                                          : Icon(Icons.favorite,
+                                              color: Colors.red))
                                 ])),
                           ],
                         ),
